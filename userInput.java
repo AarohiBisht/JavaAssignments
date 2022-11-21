750baed464f0902689173bc2cb6a74f719f06d0f
Aarohi <106364591+AarohiBisht@users.noreply.github.com>
2022-11-21 11:02:26 +0530

    Add files via upload

---
+++ userInput.java
@@ -0,0 +1,15 @@
+package InputUser;
+
+import java.util.Scanner;
+
+public class userInput {
+	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("My name is"+ name);
+
+	}
+
+}
